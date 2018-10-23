39d97cf8815989b934851ba8c8106494080a7b56
daris.lychuk <daris.lychuk@hotmail.com>
2018-10-22 22:11:42 -0600

    Finished addElement in LinkedList.java as well as printLinkedListHead

--- src/daris_lab/LinkedList.java
+++ src/daris_lab/LinkedList.java
@@ -9,4 +9,37 @@ package daris_lab;
  */
 public class LinkedList {
 
+	ListElement head; 
+	
+	public void addElement(ListElement le){
+		ListElement temp = head;
+		if(head == null){
+			head = le;
+		}
+		else
+		{
+			while(temp.getNext() != null)
+			{
+				temp = temp.getNext();
+			}
+
+			temp.setNext(le);
+		}
+	}
+	
+	public void printLinkedListHead(){
+		ListElement temp = head;
+
+		if(temp == null){
+			System.out.print("The list is currently empty.\n");
+		}else if(temp.getNext() == null){
+			System.out.print(temp.getData() + " ");
+		}else{
+			do{
+				System.out.print(temp.getData() + " ");
+				temp = temp.getNext();
+			}while(temp != null);
+
+		}
+	}
 }
